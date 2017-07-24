9b2d8dbd03f4c9cc5746c5152ee683dc21dd41c6
liujunyi <532273386@qq.com>
2017-07-24 19:28:13 +0800

    gulu

--- src/main/java/pers/ljy/background/filter/SimpleCORSFilter.java
+++ src/main/java/pers/ljy/background/filter/SimpleCORSFilter.java
@@ -13,21 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
 /***
- * 自定义过滤器
+ * 自定义跨域访问过滤器
  * @author ljy
  *
  */
 @Component
 public class SimpleCORSFilter implements Filter {
 
-	 public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-	        HttpServletResponse response = (HttpServletResponse) res;
+	 public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+	        HttpServletResponse response = (HttpServletResponse) servletResponse;
+	       // String origin = (String) servletRequest.getRemoteHost()+":"+servletRequest.getRemotePort();
 	        /* 设置浏览器跨域访问 */
 	        response.setHeader("Access-Control-Allow-Origin", "*");
-	        response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+	        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PUT");
 	        response.setHeader("Access-Control-Max-Age", "3600");
-	        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-	        chain.doFilter(req, res);
+	        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+	        response.setHeader("Access-Control-Allow-Credentials","true");
+	        filterChain.doFilter(servletRequest, servletResponse);
 	 }
 
 	 public void init(FilterConfig filterConfig) {
