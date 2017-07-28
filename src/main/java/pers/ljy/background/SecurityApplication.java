4437412fdac3be87f277dc5f03a61e011ed4d324
liujunyi <532273386@qq.com>
2017-07-28 15:11:22 +0800

    xianshi

--- src/main/java/pers/ljy/background/SecurityApplication.java
+++ src/main/java/pers/ljy/background/SecurityApplication.java
@@ -3,18 +3,25 @@ package pers.ljy.background;
 import javax.security.auth.message.config.AuthConfigFactory;
 
 import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication 
 public class SecurityApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityApplication.class);
 	public static void main(String[] args) {
 		//tomcat8.5 需要这么设置
 		if (AuthConfigFactory.getFactory() == null) {
             AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
         }
-		SpringApplication.run(SecurityApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SecurityApplication.class, args);
+		String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();  
+	    for (String profile : activeProfiles) {  
+	    	LOGGER.warn("Spring Boot 使用profile为:{}" , profile);  
+	    }
 	}
 }
 
